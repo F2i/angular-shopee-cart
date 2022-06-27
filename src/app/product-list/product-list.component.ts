7b89c3820cd3111dc0e95075cf19d8c84d7db0b9
HoangTran <Hoang.Tran@patientpop.com>
2022-06-28 02:38:37 +0700

    Next

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -6,7 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
-
+  products = [
+    {
+      name: 'Banh gio',
+      description: 'Description for product item number 1',
+      thumbnail: 'https://via.placeholder.com/200x150',
+      price: 6.99,
+      quantity: 7
+    },
+    {
+      name: 'Che dau xanh',
+      description: 'Description for product item number 2',
+      thumbnail: 'https://via.placeholder.com/200x150',
+      price: 2.99,
+      quantity: 3
+    }
+  ]
   constructor() { }
 
   ngOnInit(): void {
